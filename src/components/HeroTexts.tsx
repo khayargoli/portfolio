f61fd38553ab2e45df096567a6278e0233f7ce7d
Biswas Khayargoli <biswas.khayargoli@gmail.com>
2024-12-10 11:15:03 +0545

    Update HeroTexts.tsx

--- src/components/HeroTexts.tsx
+++ src/components/HeroTexts.tsx
@@ -6,7 +6,8 @@ const HeroTexts = () => {
   
   return (
     <>
-      <h3 className="font-poppins text-2xl max-sm:text-xl" >My Name is</h3>
+      <h2 className="font-poppins text-2xl max-sm:text-xl" >Hello!</h2>
+      <h3 className="font-poppins text-2xl max-sm:text-xl" >My name is</h3>
       <h1 className="font-rubik text-8xl name_underline text-primary max-sm:text-6xl " >
        Biswas Khayargoli<br />
       </h1>
